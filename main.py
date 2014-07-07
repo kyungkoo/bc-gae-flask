161d5552dc472963b4149b98b02dd6c574321923
Kyungkoo <kyungkoo.kang@yahoo.com>
2014-07-07 16:18:16 +0900

    change index page string to template index.html

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def main_page():
-    return "It works!"
+    return render_template('index.html')
 
 
 @app.route('/hello')
